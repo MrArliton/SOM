daae533ed931a2c7805ac873e4a629732ad6dac4
Arliton <Mr.Arliton@yandex.ru>
2019-06-14 19:54:00 +0600

    Created

---
+++ desktop/src/com/arli/som/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arli.som.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arli.som.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
